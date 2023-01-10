4bf79b36a05a2d67d854427249d61c84ca853632
David Godard <davidgodard76700@gmail.com>
2023-01-10 16:05:42 +0100

    mise à jour

--- controller.php
+++ controller.php
@@ -1,5 +1,6 @@
 <?php
 $showForm = true;
+
 $errors = [];
 $reseau = ['Facebook', 'Youtube', 'Twitter', 'Autre'];
 
@@ -7,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if (isset($_POST['lastname'])) {
         if (empty($_POST['lastname'])) {
-            $errors['lastname'] = 'c\'est vide';
+            $errors['lastname'] = 'champ obligatoire';
         }
         else if(!preg_match('/^[a-zA-ZÀ-ÿ-]+$/', $_POST['lastname'])) {
             $errors['lastname'] = 'Veuillez respecter le format';
@@ -18,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if (isset($_POST['firstname'])) {
         if (empty($_POST['firstname'])) {
-            $errors['firstname'] = 'c\'est vide';
+            $errors['firstname'] = 'champ obligatoire';
         }
         else if (!preg_match('/^[a-zA-ZÀ-ÿ-]+$/', $_POST['firstname'])) {
             $errors['firstname'] = 'Veuillez respecter le format';
@@ -32,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $errors['pseudo'] = 'ce pseudo existe déjà';
         }
         else if (empty($_POST['pseudo'])) {
-            $errors['pseudo'] = 'c\'est vide';
+            $errors['pseudo'] = 'champ obligatoire';
         }
         else if (!preg_match('/^[a-zA-ZÀ-ÿ-]+$/', $_POST['pseudo'])) {
             $errors['pseudo'] = 'Veuillez respecter le format';
@@ -43,7 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['mail'])) {
 
         if (empty($_POST['mail'])) {
-            $errors['mail'] = 'c\'est vide';
+            $errors['mail'] = 'champ obligatoire';
         }
         else if (!preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/', $_POST['mail'])) {
             $errors['mail'] = 'Veuillez respecter le format';
@@ -54,7 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if (isset($_POST['date'])) {
         if (empty($_POST['date'])) {
-            $errors['date'] = 'c\'est vide';
+            $errors['date'] = 'champ obligatoire';
         }
         else if (!preg_match('/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/', $_POST['date'])) {
             $errors['date'] = 'Veuillez respecter le format';
@@ -74,7 +75,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if (isset($_POST['password'])) {
         if (empty($_POST['password'])) {
-            $errors['password'] = 'c\'est vide';
+            $errors['password'] = 'champs obligatoire';
         }
         else if (!preg_match('/^.{8,}$/', $_POST['password'])) {
             $errors['password'] = '8 caractères minimum';
